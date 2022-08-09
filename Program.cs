49a1b52d879089c7f22fb1c6d5963ad2a90129ee
Francisco Avila <franciscoavila@Franciscos-MBP.home>
2022-08-09 15:41:02 -0700

    COMP

--- Program.cs
+++ Program.cs
@@ -6,16 +6,28 @@ namespace ExquisiteCorpse
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("What head do you want to see?");
-      string head=Console.ReadLine();
-      head=head.ToLower();
-      Console.WriteLine("What body do you want to see?");
-      string body=Console.ReadLine();
-      body=body.ToLower();
-      Console.WriteLine("What feet do you want to see?");
-      string feet=Console.ReadLine();
-      feet=feet.ToLower();
-      BuildACreature(head,body,feet);
+      Console.WriteLine("Would you like to build a creature or randomize it?");
+      Console.WriteLine("Type in YES to build or NO to randomize:");
+      string choice= Console.ReadLine();
+      choice=choice.ToUpper();
+      if (choice =="YES"){
+        Console.WriteLine("What head do you want to see?");
+        string head=Console.ReadLine();
+        head=head.ToLower();
+        Console.WriteLine("What body do you want to see?");
+        string body=Console.ReadLine();
+        body=body.ToLower();
+        Console.WriteLine("What feet do you want to see?");
+        string feet=Console.ReadLine();
+        feet=feet.ToLower();
+        BuildACreature(head,body,feet);
+      }
+      else if (choice == "NO"){
+        RandomMode();
+      }
+      else{
+        Console.WriteLine("incorrect input please try again");
+      }
     }
     static void BuildACreature(string head, string body, string feet){
       switch(head){
@@ -61,6 +73,54 @@ namespace ExquisiteCorpse
         break;
       }
     }
+    static void RandomMode(){
+      Random randomNumber = new Random();
+      int head = randomNumber.Next(1,4);
+      int body = randomNumber.Next(1,4);
+      int feet = randomNumber.Next(1,4);
+      switch(head){
+        case 1:
+          BugHead();
+        break;
+        case 2:
+          MonsterHead();
+        break;
+        case 3:
+          GhostHead();
+        break;
+        default:
+         Console.WriteLine("Wrong Input");
+        break;
+      }
+      switch(body){
+        case 1:
+          BugBody();
+        break;
+        case 2:
+          MonsterBody();
+        break;
+        case 3:
+          GhostBody();
+        break;
+        default:
+         Console.WriteLine("Wrong Input");
+        break;
+      }
+      switch (feet){
+        case 1:
+          BugFeet();
+        break;
+        case 2:
+          MonsterFeet();
+        break;
+        case 3:
+          GhostFeet();
+        break;
+        default:
+         Console.WriteLine("Wrong Input");
+        break;
+      }
+    }
     static void GhostHead()
     {
       Console.WriteLine("     ..-..");
@@ -81,7 +141,6 @@ namespace ExquisiteCorpse
       Console.WriteLine("    |     |");
       Console.WriteLine("    '~~~~~'");
     }
-
     static void BugHead()
     {
       Console.WriteLine("     /   \\");
